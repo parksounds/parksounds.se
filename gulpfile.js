955ec403e29065f6f4eec16272710895f170aa4e
Tobias Nygren <tobiasnygren@yahoo.se>
2018-03-04 12:16:29 +0100

    postcss added + minor css fixes

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,18 @@
-let gulp = require('gulp');
-let shell = require('gulp-shell');
-let browserSync = require('browser-sync').create();
-let cleanCSS = require('gulp-clean-css');
-let sourcemaps = require('gulp-sourcemaps');
+const gulp = require('gulp');
+const shell = require('gulp-shell');
+const browserSync = require('browser-sync').create();
+const sourcemaps = require('gulp-sourcemaps');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const postCSSCustomProperties = require('postcss-custom-properties');
+
+let postcss_plugins = [
+    postCSSCustomProperties(),
+    autoprefixer({browsers: ['last 1 version']}),
+    cssnano(),
+];
+
 
 gulp.task('build', shell.task(['bundle exec jekyll build']));
 
@@ -28,18 +38,19 @@ gulp.task('browser-sync', function () {
 //minify css, include source maps
 //+ stream to browser sync 
 gulp.task('minify-css-dev',() => {
-  return gulp.src('./_css/main.css')
-    .pipe(sourcemaps.init())
-    .pipe(cleanCSS())
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./'))
-    .pipe(browserSync.stream());
+
+    return gulp.src('./_css/main.css')
+        .pipe(sourcemaps.init())
+        .pipe(postcss(postcss_plugins))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./'))
+        .pipe(browserSync.stream());
 });
 
 //minify for production - no source map or browser sync
 gulp.task('minify-css',() => {
   return gulp.src('./_css/main.css')
-    .pipe(cleanCSS())
+    .pipe(postcss(postcss_plugins))
     .pipe(gulp.dest('./'));
 });
 
